9b55846e21fc7c0cb521d1949f560b2014f17d58
r00tc0d3r <wowvaltalas123@gmail.com>
2022-04-22 10:06:23 -0300

    22-04 Class

--- proyecto/index.js
+++ proyecto/index.js
@@ -3,5 +3,9 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("GET request to the homepage");
+  res.send("Hola mundo");
+});
+
+app.listen(4000, () => {
+  console.log("Listening on http://localhost:4000");
 });
