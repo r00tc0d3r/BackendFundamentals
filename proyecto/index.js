5935a58772a4a6f897f11dadd52f845cc9e4ec2d
r00tc0d3r <wowvaltalas123@gmail.com>
2022-04-25 10:04:35 -0300

    25-04 Class

--- proyecto/index.js
+++ proyecto/index.js
@@ -3,7 +3,19 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hola mundo");
+  res.send("Metodo GET");
+});
+
+app.post("/usuarios", (req, res) => {
+  res.send("Metodo POST");
+});
+
+app.put("/usuarios", (req, res) => {
+  res.send("Metodo PUT");
+});
+
+app.delete("/usuarios", (req, res) => {
+  res.send("Metodo DELETE");
 });
 
 app.listen(4000, () => {
